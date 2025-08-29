ad25944fac5021abe54f7f956778a848f51e4fe0
BadDIRT <badkarat@gmail.com>
2025-08-29 10:39:51 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <h1> halo : {{ $nama; }}</h1>
+    email : <a href="https://gmail.com/baddirtkarat@gmail.com">email saya!</a>
+    <hr>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+</body>
+</html>
