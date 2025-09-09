2b5078cbb0c2fd0b3c7b098db986d2658b24cc71
BadDIRT <badkarat@gmail.com>
2025-09-09 16:35:55 +0700

    memakai blade component dan memakai aria-current

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
-</head>
-<body>
-    <h1>artikel 1</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consectetur incidunt, et quidem, ducimus ipsam reiciendis nostrum minima commodi iusto neque numquam ipsum, amet porro voluptatibus est voluptatum? Blanditiis, minima?
-    Sunt, temporibus. Ducimus non voluptate culpa, officiis quia porro nobis veniam nihil velit consequatur architecto veritatis vel soluta in quod molestiae ea ex fugit quo aut autem itaque. Minus, dolores?
-    Quis alias sit suscipit optio blanditiis ipsum odit facilis possimus dignissimos? Sapiente sint pariatur quos. Repellendus unde iusto sequi alias ut voluptate nemo eligendi eos eveniet! Doloribus nesciunt aperiam ipsum.
-    Minima quidem magnam, illum, obcaecati incidunt similique dicta earum nemo ex eaque iste repudiandae omnis quasi, provident unde animi consectetur impedit non ab ducimus voluptatem ipsa nesciunt! Architecto, nesciunt doloremque.
-    </p>
-    <hr>
-    <h1>artikel 2</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tenetur dignissimos non vel rerum ipsum perferendis molestiae exercitationem doloribus! Temporibus aliquid corporis consequuntur, voluptatibus neque aut natus veritatis accusamus quisquam!
-    Corporis optio tenetur molestiae delectus, nihil amet tempore cum mollitia velit neque, perferendis maxime minus, voluptatem facilis assumenda alias doloribus dignissimos exercitationem. Repudiandae ut quasi dicta vel culpa sunt temporibus!
-    Consequuntur blanditiis unde, voluptate libero explicabo id laborum voluptatibus voluptates ad quas impedit illo ex architecto dolorum sunt commodi quidem. Eum, natus sequi voluptatum minus error libero nostrum harum sed!
-    </p>
-    <hr>
-    <a href="/blog">blog</a>
-    <a href="/contact">contact</a>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>contact</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
-</head>
-<body>
-    <h1> halo : {{ $nama; }}</h1>
-    email : <a href="https://gmail.com/baddirtkarat@gmail.com">email saya!</a>
-    <hr>
-    <a href="/blog">blog</a>
-    <a href="/contact">contact</a>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+</x-layout>
